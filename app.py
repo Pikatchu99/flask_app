f668b7d7ed56cbe7f475a02e19087f2a93ce7860
Modeste <yemalin.agbangla@epitech.eu>
2022-12-05 22:15:35 +0100

    [ADD] analyse data and get decision

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route('/api/v1/all', methods=['GET'])
 def hello():
     return 200, jsonify({"Equipe backend": ["Henok", "Modeste"]})
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
