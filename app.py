6dc760b79d849c6633c1104f6246f7e32491d5ba
Modeste <yemalin.agbangla@epitech.eu>
2022-12-06 07:33:45 +0100

    [ADD] analyse data and get decision

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/api/v1/all', methods=['GET'])
 def hello():
-    return 200, jsonify({"Equipe backend": ["Henok", "Modeste"]})
+    return jsonify({"Equipe backend": ["Henok", "Modeste"]})
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0')
